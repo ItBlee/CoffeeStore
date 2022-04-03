b277a0d26200f326343dce794d3a2fbaedd4c585
ASUS <tranlongtuanvu@gmail.com>
2022-04-03 17:58:57 +0700

    init database

---
+++ database/coffeestore.sql
@@ -0,0 +1,410 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2022 at 12:55 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coffeestore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ct_hoadon`
+--
+
+CREATE TABLE `ct_hoadon` (
+  `MaHD` int(10) UNSIGNED NOT NULL,
+  `MaSP` int(10) UNSIGNED NOT NULL,
+  `SoLuong` int(10) NOT NULL,
+  `DonGia` int(10) NOT NULL,
+  `TienKhuyenMai` int(10) NOT NULL,
+  `ThanhTien` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ct_khuyenmai`
+--
+
+CREATE TABLE `ct_khuyenmai` (
+  `MaKM` int(10) UNSIGNED NOT NULL,
+  `MaSP` int(10) UNSIGNED NOT NULL,
+  `GiamGia` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ct_phieunhap`
+--
+
+CREATE TABLE `ct_phieunhap` (
+  `MaPN` int(10) UNSIGNED NOT NULL,
+  `MaSP` int(10) UNSIGNED NOT NULL,
+  `SoLuong` int(10) NOT NULL,
+  `DonGia` int(10) NOT NULL,
+  `ThanhTien` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hoadon`
+--
+
+CREATE TABLE `hoadon` (
+  `MaHD` int(10) UNSIGNED NOT NULL,
+  `MaKH` int(10) UNSIGNED NOT NULL,
+  `MaNV` int(10) UNSIGNED NOT NULL,
+  `NgayLap` datetime NOT NULL DEFAULT current_timestamp(),
+  `TongTien` int(10) NOT NULL,
+  `TienKhuyenMai` int(10) NOT NULL,
+  `TienThanhToan` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `MaKH` int(10) UNSIGNED NOT NULL,
+  `Ho` varchar(50) NOT NULL,
+  `Ten` varchar(50) NOT NULL,
+  `SDT` int(20) UNSIGNED NOT NULL,
+  `DiaChi` varchar(200) NOT NULL,
+  `Email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khuyenmai`
+--
+
+CREATE TABLE `khuyenmai` (
+  `MaKM` int(10) UNSIGNED NOT NULL,
+  `TieuDe` varchar(100) NOT NULL,
+  `NoiDung` text NOT NULL,
+  `NgayBD` datetime NOT NULL,
+  `NgayKT` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loaisp`
+--
+
+CREATE TABLE `loaisp` (
+  `MaLoai` int(10) UNSIGNED NOT NULL,
+  `TenLoai` varchar(100) NOT NULL,
+  `MoTa` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nhacungcap`
+--
+
+CREATE TABLE `nhacungcap` (
+  `MaNCC` int(10) UNSIGNED NOT NULL,
+  `TenNCC` varchar(200) NOT NULL,
+  `SDT` int(20) NOT NULL,
+  `DiaChi` varchar(200) NOT NULL,
+  `SoTaiKhoan` int(20) NOT NULL,
+  `TinhTrang` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nhanvien`
+--
+
+CREATE TABLE `nhanvien` (
+  `MaNV` int(10) UNSIGNED NOT NULL,
+  `MaTK` int(10) UNSIGNED NOT NULL,
+  `Ho` varchar(50) NOT NULL,
+  `Ten` varchar(50) NOT NULL,
+  `NgaySinh` date NOT NULL,
+  `SDT` varchar(20) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `GioiTinh` tinyint(1) NOT NULL,
+  `Luong` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phieunhap`
+--
+
+CREATE TABLE `phieunhap` (
+  `MaPN` int(10) UNSIGNED NOT NULL,
+  `MaNCC` int(10) UNSIGNED NOT NULL,
+  `MaNV` int(10) UNSIGNED NOT NULL,
+  `NgayTao` datetime NOT NULL DEFAULT current_timestamp(),
+  `TongTien` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `MaSP` int(10) UNSIGNED NOT NULL,
+  `MaLoai` int(10) UNSIGNED NOT NULL,
+  `MaNCC` int(10) UNSIGNED NOT NULL,
+  `TenSP` varchar(100) NOT NULL,
+  `MoTa` text NOT NULL,
+  `HinhAnh` varchar(200) NOT NULL,
+  `DonGia` int(10) UNSIGNED NOT NULL,
+  `DonVi` varchar(50) NOT NULL,
+  `SoLuong` int(10) UNSIGNED NOT NULL,
+  `TinhTrang` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taikhoan`
+--
+
+CREATE TABLE `taikhoan` (
+  `MaTK` int(10) UNSIGNED NOT NULL,
+  `TenDangNhap` varchar(20) NOT NULL,
+  `MatKhau` varchar(20) NOT NULL,
+  `NgayTao` datetime NOT NULL DEFAULT current_timestamp(),
+  `NguoiTao` int(10) UNSIGNED NOT NULL,
+  `ChucVu` varchar(100) NOT NULL,
+  `TinhTrang` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`MaTK`, `TenDangNhap`, `MatKhau`, `NgayTao`, `NguoiTao`, `ChucVu`, `TinhTrang`) VALUES
+(1, 'admin', 'admin', '2022-04-03 17:42:55', 1, 'admin', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ct_hoadon`
+--
+ALTER TABLE `ct_hoadon`
+  ADD PRIMARY KEY (`MaHD`,`MaSP`),
+  ADD KEY `FK_CTHD_SP` (`MaSP`);
+
+--
+-- Indexes for table `ct_khuyenmai`
+--
+ALTER TABLE `ct_khuyenmai`
+  ADD PRIMARY KEY (`MaKM`,`MaSP`),
+  ADD KEY `FK_CTKM_SP` (`MaSP`);
+
+--
+-- Indexes for table `ct_phieunhap`
+--
+ALTER TABLE `ct_phieunhap`
+  ADD PRIMARY KEY (`MaPN`,`MaSP`),
+  ADD KEY `FK_CTPN_SP` (`MaSP`);
+
+--
+-- Indexes for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD PRIMARY KEY (`MaHD`),
+  ADD KEY `FK_HD_NV` (`MaNV`),
+  ADD KEY `FK_HD_KH` (`MaKH`);
+
+--
+-- Indexes for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`MaKH`);
+
+--
+-- Indexes for table `khuyenmai`
+--
+ALTER TABLE `khuyenmai`
+  ADD PRIMARY KEY (`MaKM`),
+  ADD UNIQUE KEY `UNIQUE_KM` (`TieuDe`);
+
+--
+-- Indexes for table `loaisp`
+--
+ALTER TABLE `loaisp`
+  ADD PRIMARY KEY (`MaLoai`);
+
+--
+-- Indexes for table `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  ADD PRIMARY KEY (`MaNCC`),
+  ADD UNIQUE KEY `UNIQUE_TenNCC` (`TenNCC`);
+
+--
+-- Indexes for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD PRIMARY KEY (`MaNV`),
+  ADD KEY `FK_NV_TK` (`MaTK`);
+
+--
+-- Indexes for table `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD PRIMARY KEY (`MaPN`),
+  ADD KEY `FK_PN_NCC` (`MaNCC`),
+  ADD KEY `FK_PN_NV` (`MaNV`);
+
+--
+-- Indexes for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`MaSP`),
+  ADD UNIQUE KEY `TenSP` (`TenSP`),
+  ADD KEY `FK_SP_LOAISP` (`MaLoai`),
+  ADD KEY `FK_SP_NCC` (`MaNCC`);
+
+--
+-- Indexes for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD PRIMARY KEY (`MaTK`),
+  ADD UNIQUE KEY `UNIQUE_TenDangNhap` (`TenDangNhap`) USING BTREE,
+  ADD KEY `FK_NguoiTao_MaTK` (`NguoiTao`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `khuyenmai`
+--
+ALTER TABLE `khuyenmai`
+  MODIFY `MaKM` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `loaisp`
+--
+ALTER TABLE `loaisp`
+  MODIFY `MaLoai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  MODIFY `MaNCC` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  MODIFY `MaNV` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  MODIFY `MaPN` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `MaSP` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  MODIFY `MaTK` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ct_hoadon`
+--
+ALTER TABLE `ct_hoadon`
+  ADD CONSTRAINT `FK_CTHD_HD` FOREIGN KEY (`MaHD`) REFERENCES `hoadon` (`MaHD`),
+  ADD CONSTRAINT `FK_CTHD_SP` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`);
+
+--
+-- Constraints for table `ct_khuyenmai`
+--
+ALTER TABLE `ct_khuyenmai`
+  ADD CONSTRAINT `FK_CTKM_KM` FOREIGN KEY (`MaKM`) REFERENCES `khuyenmai` (`MaKM`),
+  ADD CONSTRAINT `FK_CTKM_SP` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`);
+
+--
+-- Constraints for table `ct_phieunhap`
+--
+ALTER TABLE `ct_phieunhap`
+  ADD CONSTRAINT `FK_CTPN_PN` FOREIGN KEY (`MaPN`) REFERENCES `phieunhap` (`MaPN`),
+  ADD CONSTRAINT `FK_CTPN_SP` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`);
+
+--
+-- Constraints for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `FK_HD_KH` FOREIGN KEY (`MaKH`) REFERENCES `khachhang` (`MaKH`),
+  ADD CONSTRAINT `FK_HD_NV` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+
+--
+-- Constraints for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD CONSTRAINT `FK_NV_TK` FOREIGN KEY (`MaTK`) REFERENCES `nhanvien` (`MaNV`);
+
+--
+-- Constraints for table `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD CONSTRAINT `FK_PN_NCC` FOREIGN KEY (`MaNCC`) REFERENCES `nhacungcap` (`MaNCC`),
+  ADD CONSTRAINT `FK_PN_NV` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+
+--
+-- Constraints for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `FK_SP_LOAISP` FOREIGN KEY (`MaLoai`) REFERENCES `loaisp` (`MaLoai`),
+  ADD CONSTRAINT `FK_SP_NCC` FOREIGN KEY (`MaNCC`) REFERENCES `nhacungcap` (`MaNCC`);
+
+--
+-- Constraints for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD CONSTRAINT `FK_NguoiTao_MaTK` FOREIGN KEY (`NguoiTao`) REFERENCES `taikhoan` (`MaTK`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
