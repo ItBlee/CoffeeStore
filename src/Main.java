f3e74868c4580c537b0abac0e465950c1fb6c50e
ASUS <tranlongtuanvu@gmail.com>
2022-05-08 21:39:57 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        int i;
+        String helloFromTVU;
         Properties config = FileHandler.importConfig();
         General.importMapper(config);
         Language.setup();
