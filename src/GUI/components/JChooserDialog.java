2a3c813b1761cf39816f74e3fbbf2c4da5a599bc
DawMinh11 <105141057+DawMinh11@users.noreply.github.com>
2022-05-08 21:26:19 +0700

    ghi chú
    ghi chú abc

---
+++ src/GUI/components/JChooserDialog.java
@@ -0,0 +1,38 @@
+package GUI.components;
+
+import org.drjekyll.fontchooser.FontDialog;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+
+public class JChooserDialog {
+    /**
+     * Mở Dialog để người dùng chọn file muốn thao tác
+     * @param path đường dẫn mặc định khi mở.
+     * @return String - đường dẫn File, trả về NULL nếu hủy/thoát Dialog
+     */
+    public static String showFileChooser(String path) {
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel Files", "xls");
+        JFileChooser fileChooser = new JFileChooser(path);
+        fileChooser.setDialogTitle(Language.DIALOG_FILE_TITLE);
+        fileChooser.setFileFilter(filter);
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            return fileChooser.getSelectedFile().getAbsolutePath();
+        return null;
+    }
+
+    /**
+     * Mở Dialog để người dùng chọn font muốn thao tác
+     * @return Font
+     */
+    public static Font showFontChooser(Frame parent) {
+        FontDialog dialog = new FontDialog(parent, Language.DIALOG_FONT_TITLE, true);
+        dialog.setLocationRelativeTo(null);
+        dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        dialog.setVisible(true);
+        if (!dialog.isCancelSelected())
+            return dialog.getSelectedFont();
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
+       
         Properties config = FileHandler.importConfig();
         General.importMapper(config);
         Language.setup();
